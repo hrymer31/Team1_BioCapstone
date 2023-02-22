8ce3f54f376d983e73833a81925b91fdca30f271
AJenaeP <ajenae2u@yahoo.com>
2023-02-22 17:10:32 -0500

    Ajenae's pages

---
+++ PatientDetails.js
@@ -0,0 +1,15 @@
+//"Please Enter in your information"
+
+//Form
+// Sex at Birth: 
+    //Select : M, F
+//Height
+    //Select :
+//Body Fat %:
+    //Select :
+//Race:
+    //Select :
+//Weight:
+    //Select :
+//Target Body Weight Loss %:
+    //Select : 
---
+++ Resources.js
@@ -0,0 +1,6 @@
+//Excercise
+    //links to pages with easy/at-home/alternative workouts
+//Healthy Eating
+    //links to meal preps, healthy food options
+//Healthy Living
+    //links to pages about healthy lifestyles, 
