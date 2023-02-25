e038194f6508266e0bd7365f9c1d128bd87ba378
Jamar <69270601+harrisj1j@users.noreply.github.com>
2023-02-24 20:48:52 -0500

    Create EmailReminder.js

---
+++ EmailReminder.js
@@ -0,0 +1,21 @@
+//"Create your account"
+
+//<form onSubmit = {handleSubmit}>
+
+//label: Full Name
+
+//Input (Text): Allows users to enter their full name, and when entered sets the value to entered characters
+
+//label: Username
+
+//Input (Text): Allows users to enter a username, and when entered sets the value to entered characters
+
+//label: Password
+
+//Input (Text): Allows users to enter a password, and when entered sets the value to entered characters
+
+//label: Sign Up
+
+//Button: Sign Up button 
+
+//</form>
