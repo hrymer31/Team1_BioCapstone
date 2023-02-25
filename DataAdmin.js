e03fbd787e72ff59c6a3bdf6b26a742d91eb57f0
dleeson151 <36930889+dleeson151@users.noreply.github.com>
2023-02-25 14:08:41 -0500

    pseudo code for data admin

---
+++ DataAdmin.js
@@ -0,0 +1,79 @@
+// function ExportDataExcel (int startdate, int enddate) {
+// TODO:
+// establish connection to database
+// store selected range of data
+// export data to Excel
+//}
+
+// function ExportDataDatabase (int agestart,int ageend,int y,int exponent,int r) {
+// TODO:
+// establish connection to database
+// send passed in variables to database at specified agestart and ageend
+//}
+
+//const handleExportToExcelClick
+//TODO:
+//when clicked selected date range of data is exported to Excel
+//console.log('Exporting data to Excel...')
+
+//const handleSubmitToDatabaseClick
+//TODO:
+//user navigates to another screen to look at their account details
+//console.log('Submitting equation to database...')
+
+//<label>
+  //Export User Data
+//</label>
+
+//<label>
+  //Start:
+// <select value>
+//Day:
+//Month:
+//Year:
+//</label>
+
+//<label>
+  //End:
+// <select value>
+//Day:
+//Month:
+//Year:
+//</label>
+
+//<label>
+  //Age range equation
+//</label>
+
+//<label>
+  //Age min:
+// <select value>
+//Minimum age
+//</label>
+
+//<label>
+  //Age max:
+// <select value>
+//Maximum age
+//</label>
+
+//<label>
+  //y=
+//<input type = int>
+//</label>
+
+//<label>
+  //x-exponent
+//<input type = int>
+//</label>
+
+//<label>
+  //R=
+//<input type = int>
+//</label>
+
+//<button onClick: Export to Excel: </button>
+// data from selected date range exported to Excel
+
+//<button onClick: Submit to Database: </button>
+// new equation constants exported to database in selected age range
