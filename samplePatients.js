0a39644379b65178391ee6c40844ac6de1bfe533
audreyjallen <60330272+audreyjallen@users.noreply.github.com>
2023-03-21 21:13:20 -0400

    Test patients, added result display to PatientDetails.js

---
+++ samplePatients.js
@@ -0,0 +1,19 @@
+const malePatient = {
+    age: 30,
+    sex: 'Male',
+    race: 'Asian',
+    height: 180,
+    weightlb: 220,
+    bodyFatPerc: 24.1,
+    targetWeightLossPerc: 10.0
+}
+
+const femalePatient = {
+    age: 23,
+    sex: 'Female',
+    race: 'White',
+    height: 167,
+    weightlb: 180,
+    bodyFatPerc: 32.0,
+    targetWeightLossPerc: 22.2
+}
