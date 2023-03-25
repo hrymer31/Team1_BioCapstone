738168450ce6329738058de4363fdd5f2b43e559
audreyjallen <60330272+audreyjallen@users.noreply.github.com>
2023-03-25 15:05:22 -0400

    Added age check and disqualification pages (JS)

---
+++ steppingforward/src/Pages/ageCheck.js
@@ -0,0 +1,58 @@
+import * as React from 'react'
+import { useState } from 'react'
+import { FormControl } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import './agePages.css'
+
+const options = ["Yes", "No"]
+
+export const AgeCheck = () => {
+
+    const [choice, setChoice] = useState(null)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //Navigation goes here
+        //If yes was selected --> proceed with making account
+        //If no was selected --> navigate to disqualification page
+    }
+
+    return (
+        <div className = "ageCheck">
+            <Typography variant = "h3" gutterBottom>Before we begin...</Typography>
+
+            <Typography variant = "h6" gutterBottom>Are you between the ages of 19 and 40?</Typography>
+
+            <FormControl className = "selectBox" margin = "normal">
+
+                <InputLabel id="demo-simple-select-age-range">Select</InputLabel>
+                    <Select
+                    labelId="demo-simple-select-age-range"
+                    id="demo-simple-select-age-range"
+                    defaultValue=""
+                    value={choice}
+                    label="Choice"
+                    onChange={(e) => setChoice(e.target.value)}
+                    >
+                        {options.map(ageRange => {
+                            return (
+                                <MenuItem value={ageRange}>
+                                    {ageRange}
+                                </MenuItem>
+                            )
+                        })}
+                    </Select>
+            </FormControl>
+            <button 
+            className = "btn"
+            type = "submit"
+            onSubmit={handleSubmit}
+            >
+                Submit
+            </button>
+        </div>
+    )
+}
---
+++ steppingforward/src/Pages/disqualify.js
@@ -0,0 +1,72 @@
+import * as React from 'react'
+import { useState } from 'react'
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import './agePages.css'
+
+export const Disqualify = () => {
+
+    const [email, setEmail] = useState(null)
+    const [age, setAge] = useState(null)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //Store information in database
+    }
+
+    return (
+        <div className = "disqualify">
+
+            <Typography variant = "h3" gutterBottom>Thank you for your interest!</Typography>
+            <Typography variant = "h6">We're sorry, but your age does not qualify for this study.</Typography>
+            <Typography variant = "h6">If further research extends into your age range,</Typography>
+            <Typography variant = "h6">would you like us to contact you to participate?</Typography>
+            <Typography variant = "h6" marginTop={5}>If so, please enter your information below:</Typography>
+
+        <div className = "disqualifyForm">
+
+            <div className = "formText">
+                <Typography variant = "p">Please enter your email:</Typography>
+            </div>
+            <div className = "formField">
+                <TextField
+                size = "small"
+                variant = "outlined"
+                type = "email"
+                value = {email}
+                required
+                onChange = {(e) => setEmail(e.target.value)}
+                />
+            </div>
+
+        </div>
+
+        <div className = "disqualifyForm">
+
+            <div className = "formText">
+                <Typography variant = "p">Please enter your age:</Typography>
+            </div>
+            <div className = "formField">
+                <TextField
+                size = "small"
+                variant = "outlined"
+                type = "number"
+                value = {age}
+                required
+                onChange = {(e) => setAge(e.target.value)}
+                />
+            </div>
+
+        </div>
+
+        <button
+        className = "btn"
+        type = "submit"
+        onSubmit = {handleSubmit}
+        >
+            Submit
+        </button>
+
+        </div>
+    )
+}
