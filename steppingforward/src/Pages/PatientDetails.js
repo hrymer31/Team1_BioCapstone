cecbdd9a9809b381a1462952d141822f3fb7be8a
AJenaeP <ajenae2u@yahoo.com>
2023-04-07 14:19:56 -0400

    update to resources and formula

--- steppingforward/src/Pages/PatientDetails.js
+++ steppingforward/src/Pages/PatientDetails.js
@@ -4,7 +4,6 @@ import {
     FormControl, 
     Button
  } from '@mui/material'
-import Quotes from '../InspirationalQuotes';
 import { Box } from '@mui/system';
 
 const ageArray = [];
@@ -26,11 +25,6 @@ const PatientDetails = () => {
     const [age, setAge] = useState()
     const [sex, setSex] = useState()
     const [race, setRace] = useState()
-    const [height, setHeight] = useState()
-
-    const [weightlb, setWeightlb] = useState()
-    const [bodyFatPerc, setBodyFatPerc] = useState()
-    const [targetWeightLossPerc, setTargetWeightLossPerc] = useState()
 
     const [ageError, setAgeError] = useState(false)
     const [sexError, setSexError] = useState(false)
@@ -39,88 +33,83 @@ const PatientDetails = () => {
     const [weightError, setWeightError] = useState(false)
     const [bodyFatError, setBodyFatError] = useState(false)
     const [targetWeightError, setTargetWeightError] = useState(false)
- 
-    const [weightkg, setWeightkg] = useState(null)
-    const [currentFatMass, setCurrentFatMass] = useState(null)
-    const [currentFatFreeMass, setCurrentFatFreeMass] = useState(null)
-    const [targetWeightLosskg, setTargetWeightLosskg] = useState(null)
-    const [targetBodyWeightkg, setTargetBodyWeightkg] = useState(null)
-    const [newFatMass, setNewFatMass] = useState(null)
-    const [targetBodyFatPerc, setTargetBodyfatPerc] =  useState(null)
-    const [stepsPerDay, setStepsPerDay] = useState(null)
-    const [results, setResults] = useState(null)
+
+    const [result, setResult] = useState(null)
+    const [userDetails, setUserDetails] = useState({
+        age: 0,
+        sex: '',
+        race: '',
+        height: 0,
+        weightlb: 0,
+        bodyFatPerc: 0,
+        targetWeightLossPerc: 0,
+        weightkg: 0,
+        currentFatMass: 0,
+        currentFatFreeMass: 0,
+        targetWeightLosskg: 0,
+        targetBodyWeightkg: 0,
+        newFatMass: 0,
+        targetBodyFatPerc: 0,
+        stepsPerDay: 0,
+        totalStepTarget: 0
+    })
+    function handleAgeChange(e) { userDetails.age = Number(e.target.value); setAge(Number(e.target.value));}
+    function handleSexChange(e) { userDetails.sex = e.target.value; setSex(e.target.value);}
+    function handleRaceChange(e) { userDetails.race = e.target.value; setRace(e.target.value);}
+    function handleHeightChange(e) { userDetails.height = Number(e.target.value);}
+    function handleWeightChange(e) { userDetails.weightlb = Number(e.target.value);}
+    function handleBodyFatChange(e) { userDetails.bodyFatPerc = Number(((e.target.value) / 100));}
+    function handleTargetWeightChange(e) { userDetails.targetWeightLossPerc = Number(((e.target.value) / 100));}
 
     const validate = () => {
-        if(age === undefined){
-            setAgeError(true)
-        }
-        if(sex === undefined){
-            setSexError(true)
-        }
-        if(race === undefined){
-            setRaceError(true)
-        }
-        if(height === undefined) {
-            setHeightError(true)
-        }
-        if (weightlb === undefined) {
-            setWeightError(true)
-        }
-        if (bodyFatPerc === undefined) {
-            setBodyFatError(true)
-        }
-        if (targetWeightLossPerc === undefined) {
-            setTargetWeightError(true)
-        }
+        console.log(userDetails)
+        if(userDetails.age === 0){ setAgeError(true) }
+        if(userDetails.sex === ''){ setSexError(true) }
+        if(userDetails.race === ''){ setRaceError(true) }
+        if(userDetails.height === 0){ setHeightError(true) }
+        if(userDetails.weightlb === 0){ setWeightError(true) }
+        if(userDetails.bodyFatPerc === 0){ setBodyFatError(true) }
+        if(userDetails.targetWeightLossPerc === 0){ setTargetWeightError(true) }
         if(
-            (age !== undefined) && 
-            (sex !== undefined) && 
-            (race !== undefined) &&
-            (height !== undefined) && 
-            (weightlb !== undefined) && 
-            (bodyFatPerc !== undefined) &&
-            (targetWeightLossPerc !== null)
+            (userDetails.age !== 0) && 
+            (userDetails.sex !== '') && 
+            (userDetails.race !== '') &&
+            (userDetails.height !== 0) && 
+            (userDetails.weightlb !== 0) && 
+            (userDetails.bodyFatPerc !== 0) &&
+            (userDetails.targetWeightLossPerc !== 0)
         ){
-            calculate()
+             calculate()
         }
     }
     const calculate = () => {
-            setWeightkg(
-                (weightlb / 2.20462)
-            )
-            setCurrentFatMass(
-                (weightkg * bodyFatPerc)
-            )
-            setCurrentFatFreeMass(
-                (weightkg - currentFatMass)
-            )
-            setTargetWeightLosskg(
-                (weightkg * targetWeightLossPerc)
-            )
-            setTargetBodyWeightkg(
-                (weightkg - targetWeightLosskg)
-            )
-            setNewFatMass(
-                (currentFatMass - targetWeightLosskg)
-            )
-            setTargetBodyfatPerc(
-                ((newFatMass / targetBodyWeightkg) * 100)
-            )            
-            if (sex === 'Male') {
-                const results = ((39377.3357744) / (Math.pow(targetBodyFatPerc, 1.304048257)))
-                setResults(results)
-            } else if (sex === 'Female') {
-                const results = ((261425.44) / (Math.pow(targetBodyFatPerc, 1.87969924)))
-                setResults(results)
-            }  
-            setAgeError(false)
-            setSexError(false)
-            setRaceError(false)
-            setHeightError(false)
-            setWeightError(false)
-            setBodyFatError(false)
-            setTargetWeightError(false)   
+        userDetails.weightkg = (userDetails.weightlb / 2.20462) ;
+        userDetails.currentFatMass = (userDetails.weightkg * userDetails.bodyFatPerc);
+        userDetails.currentFatFreeMass = (userDetails.weightkg - userDetails.currentFatMass);
+        userDetails.targetWeightLosskg = (userDetails.weightkg * userDetails.targetWeightLossPerc);
+        userDetails.targetBodyWeightkg = (userDetails.weightkg - userDetails.targetWeightLosskg);
+        userDetails.newFatMass = (userDetails.currentFatMass - userDetails.targetWeightLosskg);
+        userDetails.targetBodyFatPerc = ((userDetails.newFatMass / userDetails.targetBodyWeightkg)*100);
+
+        if (sex === 'Male') {
+            userDetails.stepsPerDay = ((39377.3357744) / (Math.pow(userDetails.targetBodyFatPerc, 1.304048257)))
+            setResult((39377.3357744) / (Math.pow(userDetails.targetBodyFatPerc, 1.304048257)))
+        } else if (sex === 'Female') {
+            userDetails.stepsPerDay = ((261425.44) / (Math.pow(userDetails.targetBodyFatPerc, 1.87969924)))
+            setResult((261425.44) / (Math.pow(userDetails.targetBodyFatPerc, 1.87969924)))
+        }
+        
+        userDetails.totalStepTarget = (userDetails.stepsPerDay * userDetails.currentFatMass)
+
+        setAgeError(false)
+        setSexError(false)
+        setRaceError(false)
+        setHeightError(false)
+        setWeightError(false)
+        setBodyFatError(false)
+        setTargetWeightError(false)   
     };
+
     return (
         <div className='PatientDetails'>
             <Box 
@@ -144,7 +133,7 @@ const PatientDetails = () => {
                     <div className='form'>
                         <div className='inputSection'>
                             <label htmlFor="age">Age</label>
-                            <select id="age" value={age} onChange={(e) => setAge(e.target.value)}>
+                            <select id="age" value={age} onChange={(e) => handleAgeChange(e)}>
                                 <option value=""></option>
                                 {
                                     ageArray.map(ageOption => {
@@ -160,7 +149,7 @@ const PatientDetails = () => {
                         }    
                         <div className='inputSection'>
                             <label htmlFor="sex">Sex</label>
-                            <select id='sex' value={sex} onChange={(e) => setSex(e.target.value)}>
+                            <select id='sex' value={sex} onChange={(e) => handleSexChange(e)}>
                                 <option value=""></option>
                                 {sexArray.map(sexOption => {
                                     return (
@@ -174,7 +163,7 @@ const PatientDetails = () => {
                         }
                         <div className='inputSection'>
                             <label htmlFor="race">Race</label>
-                            <select id='race' value={race} onChange={(e) => setRace(e.target.value)}>
+                            <select id='race' value={race} onChange={(e) => handleRaceChange(e)}>
                                 <option value=""></option>
                                 {
                                     raceArray.map(raceOption => {
@@ -190,7 +179,7 @@ const PatientDetails = () => {
                         }
                         <div className='inputSection'>
                             <label htmlFor='height'>Height(cm)</label>
-                            <input id='height' type='number' placeholder='cm' onChange={(e) => setHeight(e.target.value)} /> 
+                            <input id='height' type='number' placeholder='cm' onChange={(e) => handleHeightChange(e)} /> 
                         </div>
                         {
                             (heightError) && <div style={{ color: 'red' }} className='error'>You must select a valid height</div>
@@ -198,21 +187,21 @@ const PatientDetails = () => {
                         <p> Enter all calculations below to the nearest tenths place (e.g. 250.5, 22.0)</p>
                         <div className='inputSection'>
                             <label htmlFor='weight'>Weight(lb)</label>
-                            <input id='weight' type='number' placeholder='lb' onChange={(e) => setWeightlb(e.target.value)} /> 
+                            <input id='weight' type='number' placeholder='lb' onChange={(e) => handleWeightChange(e)} /> 
                         </div>
                         {
                             (weightError) && <div style={{ color: 'red' }} className='error'>You must select a valid weight</div>
                         }
                         <div className='inputSection'>
                             <label form='bodyFatPerc'>Body Fat %</label>
-                            <input id='bodyFatPerc' type='number' placeholder='%' onChange={(e) => setBodyFatPerc((e.target.value) / 100)} />
+                            <input id='bodyFatPerc' type='number' placeholder='%' onChange={(e) => handleBodyFatChange(e)} />
                         </div>
                         {
                             (bodyFatError) && <div style={{ color: 'red' }} className='error'>You must select a valid body fat %</div>
                         }
                         <div className='inputSection'>
                             <label form='targetWeightLossPerc'>Target Weight Loss %</label>
-                            <input id='targetWeightLossPerc' type='number' placeholder='%' onChange={(e) => setTargetWeightLossPerc((e.target.value) / 100)} />
+                            <input id='targetWeightLossPerc' type='number' placeholder='%' onChange={(e) => handleTargetWeightChange(e)} />
                         </div>
                         {
                             (targetWeightError) && <div style={{ color: 'red' }} className='error'>You must select a valid target weight %</div>
@@ -224,9 +213,12 @@ const PatientDetails = () => {
                         Get Results!
                     </Button>
                 </FormControl>    
-                <div className='result'>
-                    Prescribed daily step count: {Math.trunc(results)}
-                </div>  
+                {
+                    (result !== null) && 
+                    <div className='result'>
+                        Prescribed daily step count: {Math.trunc(result)}
+                    </div>  
+                }
             </Box>                           
         </div>
     )
--- steppingforward/src/Pages/Resources.js
+++ steppingforward/src/Pages/Resources.js
@@ -3,6 +3,7 @@ import '../Css/Resources.css'
 import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
 import LocalDrinkIcon from '@mui/icons-material/LocalDrink';
 import RestaurantIcon from '@mui/icons-material/Restaurant';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
 import Link from '@mui/material/Link'
 import { Box } from '@mui/system';
 
@@ -15,12 +16,18 @@ import { Box } from '@mui/system';
                     <div className='steps'>
                         <DirectionsWalkIcon className='resourcesIcon'></DirectionsWalkIcon>
                         <Link 
+                            className='link'
+                            target={'_blank'}
                             href='https://blog.myfitnesspal.com/ways-to-add-1000-more-steps-to-your-day/'
                         >15 Ways to Add 1,000 More Steps</Link>
-                        <Link 
+                        <Link
+                            className='link' 
+                            target={'_blank'}
                             href='https://www.piedmont.org/living-better/30-easy-ways-get-more-steps-each-day'
                         >30 easy ways to get more steps each day </Link>
-                        <Link 
+                        <Link
+                            className='link'
+                            target={'_blank'} 
                             href='https://www.eatthis.com/fun-ways-to-lose-weight/'
                         >35 fun activies that dont feel like exercise</Link>
                     </div>
@@ -29,12 +36,18 @@ import { Box } from '@mui/system';
                     <div className='water'>
                         <LocalDrinkIcon className='resourcesIcon'></LocalDrinkIcon>
                         <Link 
+                            className='link'
+                            target={'_blank'}
                             href='https://hub.jhu.edu/at-work/2020/01/15/focus-on-wellness-drinking-more-water/#:~:text=Science%20suggests%20that%20water%20can,to%20results%20on%20the%20scale.'
                         >How drinking more water can help you lose more weight</Link>
                         <Link 
+                            className='link'
+                            target={'_blank'}
                             href='https://www.waterlogic.com/en-gb/resources/blog/ten-ways-make-drinking-water-interesting/'
                         >10 ways to make drinking water more interesting</Link>
                         <Link 
+                            className='link'
+                            target={'_blank'}
                             href='https://www.health.harvard.edu/staying-healthy/how-much-water-should-you-drink'
                         >How much water should you drink a day?</Link>
                     </div>
@@ -43,16 +56,42 @@ import { Box } from '@mui/system';
                     <div className='food'>
                         <RestaurantIcon className='resourcesIcon'></RestaurantIcon>
                         <Link 
+                            className='link'
+                            target={'_blank'}
                             href='https://gatheringdreams.com/easy-healthy-meal-prep-ideas-30-minutes/'
                         >25+ Simple Meal Prep Ideas for Weight Loss</Link>
                         <Link 
+                            className='link'
+                            target={'_blank'}
                             href='https://www.healthline.com/nutrition/how-to-meal-prep'
                         >How to Meal Prep</Link>
                         <Link 
+                            className='link'
+                            target={'_blank'}
                             href='https://www.cdc.gov/nccdphp/dnpao/features/healthy-eating-tips/index.html'
                         >Tips for eating healthier</Link>
                     </div>
                 </Box>    
+                <Box className="resourceBox article">
+                    <div className='article'>
+                        <NewspaperIcon className='resourcesIcon'></NewspaperIcon>
+                        <Link 
+                            className='link'
+                            target={'_blank'}
+                            href="https://bmjopen.bmj.com/content/7/3/e011843.long"
+                        >Association between physical activity and body fat percentage...</Link>
+                        <Link 
+                            className='link'
+                            target={'_blank'}
+                            href="https://bmcpublichealth.biomedcentral.com/articles/10.1186/s12889-015-1381-6"
+                        >The association between daily steps and health...</Link>
+                        <Link 
+                            className='link'
+                            target={'_blank'}
+                            href="https://journals.lww.com/acsm-msse/Fulltext/2004/05000/Relationship_between_Accumulated_Walking_and_Body.26.aspx"
+                        >Relationship between Accumulated Walking and Body Composition in Middle-Aged Women</Link>
+                    </div>
+                </Box>
             </div>
         </div>
     )
