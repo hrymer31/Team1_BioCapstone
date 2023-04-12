575e93b438505cc9a473febadc5aac38a276da34
Jamar <69270601+harrisj1j@users.noreply.github.com>
2023-04-12 13:48:02 -0400

    Update Navbar.js

--- steppingforward/src/Pages/Navbar.js
+++ steppingforward/src/Pages/Navbar.js
@@ -19,7 +19,8 @@ export default function Navbar() {
       <CustomLink to="/details"> Calculate Steps </CustomLink>  
       <CustomLink to="/resources"> Resources </CustomLink>  
       <CustomLink to="/goals"> Goals </CustomLink> 
-      <CustomLink to="/reminders"> Email Reminders </CustomLink>   
+      //<CustomLink to="/reminders"> Email Reminders </CustomLink>   
+  <CustomLink to="/contactForm"> Email Reminders </CustomLink>  
       <CustomLink to="/profile"> My Account </CustomLink> 
       <CustomLink to="/logout"> Log Out </CustomLink>    
     
