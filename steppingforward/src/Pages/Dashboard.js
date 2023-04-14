1f00f113944881a6f2edaeb70d42f51bc97d801a
hrymer31 <123103874+hrymer31@users.noreply.github.com>
2023-04-14 15:59:28 -0400

    Updates to Dashboard

--- steppingforward/src/Pages/Dashboard.js
+++ steppingforward/src/Pages/Dashboard.js
@@ -5,27 +5,69 @@ import '../Css/dashboard.css'
 import { Box } from '@mui/system';
 import Link from '@mui/material/Link'
 import Navbar from './Navbar';
+import {
+  FormControl,
+  Button
+} from '@mui/material'
 
 const Dashboard = () => {
   return (
     <>
-    <Navbar />
-      <div className="Dashboard" id="outer-container">
+      <Navbar />
+      <p> <Quotes /> </p>
+      <Box
+        className="dashboard"
+        sx={{
+          backgroundColor: 'white',
+          width: 600,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          height: 400,
+          overflow: 'auto',
+          borderRadius: 2
+        }}>
+        <FormControl
+          fullWidth
+          sx={{
+            display: 'flex'
+          }}
+        >
+          <h1>Your Results: </h1>
 
-        <Quotes />
 
-        <div className='sections'>
-          <Box className="dashboardBox steps">
-           
-            <p> Daily Step Count </p>
+
+          <div className='sections'>
+            <Box className="dashboardBox steps">
+
+              <p> Current Step Count:   </p>
+            </Box>
+
+            <Box className="dashboardBox steps">
+
+              <p> Your Current Weight:  </p>
             </Box>
             <Box className="dashboardBox weight">
-            
-            <p> Current Weight </p>
-            </Box>
-        </div>
-      </div>
 
+              <p> Prescribed Steps: </p>
+            </Box>
+            <Button
+            //onClick={update}
+            >
+              Add Steps
+            </Button>
+
+        
+          <Button
+          //onClick={handleClick}
+          >
+            Update Weight
+          </Button>
+          </div>
+        </FormControl>
+      
+
+      </Box>
+    
 
     </>
   );
