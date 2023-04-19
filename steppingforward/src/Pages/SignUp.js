9acabc93c0fe888514e0f526363766ffd0bae50d
Jamar <69270601+harrisj1j@users.noreply.github.com>
2023-04-19 12:42:01 -0400

    Updated SignUp.js
    added password requirements and when requirements are met they are slashed out, when users hit the submit request button it redirects them to the login page

--- steppingforward/src/Pages/SignUp.js
+++ steppingforward/src/Pages/SignUp.js
@@ -1,5 +1,4 @@
 import React , {useEffect, useState} from "react";
-//import PasswordChecklist from "react-password-checklist"
 import Select from 'react-select';
 import { UserAuth } from './AuthContext'
 import { useNavigate } from "react-router-dom";
@@ -10,25 +9,28 @@ import ClearIcon from '@mui/icons-material/Clear';
 import 'simplebar-react/dist/simplebar.min.css';
 
 const SignUp = () => {
+    const [passwordStrength, setPasswordStrength] = useState(0);
     const style = {
         width: 500,
     }
     
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [goToHome, setgoToHome] = useState(false);
-   if (goToHome) {
-      navigate('/ageCheck')
-  }; 
+    const [goToLogin, setgoToLogin] = useState(false);
+    if (goToLogin) {
+        navigate('/login')
+    }; 
 
-  const [name, setName] = useState("")
-  const [UserId, setUserId] = useState("")
-  const [Email, setEmail] = useState("")
-  const [Password, setPassword] = useState("")
-  const [error, setError] = useState("")
-  const [passwordAgain, setPasswordAgain] = useState("")
+    const [name, setName] = useState("")
+    const [UserId, setUserId] = useState("")
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+    const [error, setError] = useState("")
+    const [passwordAgain, setPasswordAgain] = useState("")
+    const [alreadySignedUp, setAlreadySignedUp] = useState(false);
 
-  const { createUser } = UserAuth();
+
+    const { createUser } = UserAuth();
 
     const userInfo = {
         Name: name,
@@ -36,58 +38,85 @@ const SignUp = () => {
     }
 
     const handleSumbit = async (e) => {
-      e.preventDefault()
-      setError("")
-      try {
-          await createUser(Email, Password, userInfo);
-          navigate('/home')
-      } catch (e) {
-          setError(e.message)
-          console.log(e.message)
+        e.preventDefault()
+        setError("")
+        if (passwordStrength < 2) {
+          setError("Password does not meet requirements")
+          return;
+        }
+        try {
+            await createUser(Email, Password, userInfo);
+            navigate('/home')
+        } catch (e) {
+            setError(e.message)
+            console.log(e.message)
+        }
       }
-  }
+      
+      
+      
+
+    useEffect(() => {
+        const calculatePasswordStrength = () => {
+            let strength = 0;
+            if (Password.length >= 6) {
+                strength += 1;
+            }
+            if (/[A-Z]/.test(Password)) {
+                strength += 1;
+            }
+            setPasswordStrength(strength);
+        }
+        calculatePasswordStrength();
+    }, [Password]);
 
   return (
     <div className="SignUp">
-      <h1>Sign Up</h1>
-      <Container className="form" style={{width: 800}}>
-      <div id='formHeader'>
-      </div>
-      <FormControl id="formcontrol" onSubmit={handleSumbit}>
-                    <div id="personalInfo">
-                        <TextField
-                            id="outlined-password-input Name"
-                            label="Name"
-                            type="text"
-                            style={{ marginRight: 10,}}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                        <TextField
-                            id="outlined-password-input Email"
-                            label="Email"
-                            type="text"
-                            onChange={(e) => setEmail(e.target.value)}
-                        /> 
+        <h1>Sign Up</h1>
+        <Container className="form" style={{width: 800}}>
+            <div id='formHeader'></div>
+            <FormControl id="formcontrol" onSubmit={handleSumbit}>
+                <div id="personalInfo">
+                    <TextField
+                        id="outlined-password-input Name"
+                        label="Name"
+                        type="text"
+                        style={{ marginRight: 10,}}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <TextField
+                        id="outlined-password-input Email"
+                        label="Email"
+                        type="text"
+                        onChange={(e) => setEmail(e.target.value)}
+                    /> 
+                </div>
+                <div id="passwords">
+                    <TextField
+                        id="outlined-password-input password"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                        style={{marginRight: 10}}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <TextField
+                        id="outlined-password-input password"
+                        label="Confirm Password"
+                        type="password"
+                        autoComplete="current-password"
+                        style={{ marginRight: 10 }}
+                        onChange={e => setPasswordAgain(e.target.value)}
+                    />
+                    <div id="password-requirements">
+                        <h4>Password Requirements:</h4>
+                        <ul>
+                            <li style={{ textDecoration: passwordStrength >= 1 ? 'line-through' : 'none' }}>At least one uppercase letter</li>
+                            <li style={{ textDecoration: passwordStrength >= 2 ? 'line-through' : 'none' }}>Must be at least 6 characters</li>
+
+                        </ul>
                     </div>
-                    <div id="passwords">
-                        <TextField
-                            id="outlined-password-input password"
-                            label="Password"
-                            type="password"
-                            autoComplete="current-password"
-                            style={{marginRight: 10}}
-                            onChange={e => setPassword(e.target.value)}
-                        />
-                        <TextField
-                            id="outlined-password-input password"
-                            label="Confirm Password"
-                            type="password"
-                            autoComplete="current-password"
-                            style={{ marginRight: 10 }}
-                            onChange={e => setPasswordAgain(e.target.value)}
-                        />
-                        
-                    </div>
+                </div>
                     
                     
                     <div id="buttons" style={{ marginTop: -10 }} >
@@ -98,7 +127,7 @@ const SignUp = () => {
                                 style={{width: 225, marginRight: 10}}
                                 startIcon={<DoneIcon />}
                                 className='submit'
-                                onClick={() => { setgoToHome(true) }}
+                                onClick={() => { setgoToLogin(true) }}
                                 sx={{ ':hover': { bgcolor: 'rgb(161, 252, 134,0.2)' } }}
                             >
                                 Sumbit Request
@@ -109,7 +138,7 @@ const SignUp = () => {
                                 style={{ width: 225, hover: {backgroundColor: 'red'} }} 
                                 startIcon={<ClearIcon />}
                                 className='cancel'
-                                onClick={() => { setgoToHome(true) }}
+                                onClick={() => { setgoToLogin(true) }}
                                 sx={{ ':hover': { bgcolor: 'rgb(252, 83, 83,0.2)' } }}
                             >
                                 Cancel
